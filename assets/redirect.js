991d4fcf192c6b978bb748ca581c22548d09f6ed
ClailsonSantos <167331908+ClailsonSantos@users.noreply.github.com>
2024-11-28 00:21:07 -0300

    Revert "Backup v1.6"

--- assets/redirect.js
+++
@@ -1,9 +0,0 @@
-const butaummassa = document.querySelectorAll('.btn');
-
-butaummassa.forEach(butaum => {
-    butaum.addEventListener('click', (event) => {
-        event.preventDefault();
-        const targetId = butaum.getAttribute('target');
-        window.location.href = `pag2.html#${targetId}`;
-    }); 
-});
