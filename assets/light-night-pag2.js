8410841eed8c02450cd9e66fe7adcb5fd23ec345
MatheusNascimento87 <pn_matheus@outlook.com>
2024-11-27 22:48:17 -0300

    Adicionado a pagina de galeria

---
+++ assets/light-night-pag2.js
@@ -0,0 +1,53 @@
+function trocarCor() {
+    let header = document.querySelector('.header');
+    let text = document.querySelectorAll('.link'); //todos os que tiverem a classe link
+    let footer = document.querySelector('.footer');
+    let loginColor = document.querySelector('.cards');
+    let descricao = document.querySelectorAll('.descricao')
+
+    if(header.style.backgroundColor == `white`){
+      header.style.backgroundColor = 'rgb(63, 63, 63)';
+
+      footer.style.backgroundColor = 'rgb(59, 59, 59)';
+      footer.style.color = 'white';
+
+      loginColor.style.backgroundColor = `rgba(63, 63, 63, 0.9)`;
+
+      text.forEach((text) => {
+        text.style.color = 'white';
+      });
+
+      descricao.forEach((loginTexts) => {
+        loginTexts.style.color ='white';
+      });
+
+    } else{
+      header.style.backgroundColor = `white`;
+
+      footer.style.backgroundColor = 'white';
+      footer.style.color = 'rgb(59, 59, 59)';
+
+      loginColor.style.backgroundColor = `rgba(255,255,255,0.9)`;
+
+      text.forEach((text) => {
+        text.style.color = 'black';
+      });
+
+      descricao.forEach((loginTexts) => {
+        loginTexts.style.color ='rgba(63, 63, 63)';
+      });
+      
+    }
+  
+    const btnColorSite = document.querySelector('.btn-color-site, .btnColorSiteBlack');
+  
+    if (btnColorSite) {
+      if (btnColorSite.classList.contains('btn-color-site')) {
+        btnColorSite.classList.remove('btn-color-site');
+        btnColorSite.classList.add('btnColorSiteBlack');
+      } else {
+        btnColorSite.classList.remove('btnColorSiteBlack');
+        btnColorSite.classList.add('btn-color-site');
+      }
+    }
+  }
